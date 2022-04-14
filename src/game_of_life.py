8340cac3f09bcc3025b71bd65f94d93bc1651b57
bion <bionj@google.com>
2022-04-14 15:28:15 -0700

    WIP

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,6 +1,23 @@
 #! /usr/bin/env python3
 
+class Board():
+
+    def __init__(self, cells):
+        self.cells = cells
+
+
 class GameOfLife():
+
+    def parse_pattern_file(self, pattern_string):
+        coordinates = pattern_string.split(" ")
+
+    def __init__(self, pattern_path):
+        with open(pattern_path, "r") as f:
+            for line in f:
+
+
+        self.board = Board(cells)
+
     def run(self, state):
         pass
 
