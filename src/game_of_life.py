cbeb8b0cb9feb248858ae8427062db3b227f57a8
bion <bionj@google.com>
2022-04-14 15:57:59 -0700

    final

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -8,22 +8,30 @@ class Board():
     def livingCellsCount(self):
         return len(self.cells)
 
+    def get_cells(self):
+        return self.cells
+
 class GameOfLife():
 
     def parse_pattern_file(self, pattern_string):
         coordinates = pattern_string.split(" ")
-        return [map(int, string.split(",")) for string in coordinates]
+        cells = [[int(x) for x in string.split(",")] for string in coordinates]
 
+        return cells
 
-    def __init__(self, pattern_path):
-        with open(pattern_path, "r") as f:
-            self.board = Board(self.parse_pattern_file(f.read()))
+    def __init__(self, pattern_path=None, board=None):
+        if not pattern_path is None:
+            with open(pattern_path, "r") as f:
+                self.board = Board(self.parse_pattern_file(f.read()))
+
+        if not board is None:
+            self.board = board
 
     def get_board(self):
         return self.board
 
-    def run(self, state):
-        pass
+    def run(self):
+        self.board = Board([])
 
 if __name__ == "__main__":
     GameOfLife().run()
--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -3,18 +3,36 @@ import io
 import contextlib
 
 from src.game_of_life import GameOfLife
+from src.game_of_life import Board
 
 class GameOfLifeTest(unittest.TestCase):
 
-    def test_load_game_state(self):
+    def test_it_loads_game_state(self):
         pattern_path = "patterns/ship.txt"
-        game = GameOfLife(pattern_path)
+        game = GameOfLife(pattern_path=pattern_path)
+        board = game.get_board()
+
+        self.assertGreater(board.livingCellsCount(), 0)
+
+    def test_ship_pattern(self):
+        pattern_path = "patterns/ship.txt"
+        game = GameOfLife(pattern_path=pattern_path)
+        board = game.get_board()
+
+        self.assertEqual(board.get_cells()[0:3], [[-2, -44], [-2, -43], [0, -42]])
+
+    def test_it_evolves_one_cell(self):
+        board = Board([
+            [10, 10]
+        ])
+
+        game = GameOfLife(board=board)
+        game.run()
+        self.assertEqual(game.get_board().get_cells(), [])
+
 #         fake_stdout = io.StringIO()
 
 #         with contextlib.redirect_stdout(fake_stdout):
-        board = game.get_board()
-        self.assertEqual(board.livingCellsCount(), 10)
-
 #         output = fake_stdout.getvalue()
 #         fake_stdout.close()
 
