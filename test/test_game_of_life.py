b2889a926eee2dc4c19dd802833744449bc896a7
Susan Goldblatt <sgoldblatt@google.com>
2022-04-14 14:48:55 -0700

    adding initial tests

--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -6,14 +6,38 @@ from src.game_of_life import GameOfLife
 
 class GameOfLifeTest(unittest.TestCase):
 
-    def test_run_doesnt_do_much(self):
+    def test_single_cell_dies(self):
         game = GameOfLife()
         fake_stdout = io.StringIO()
 
         with contextlib.redirect_stdout(fake_stdout):
-            game.run()
+            game.run("1,1")
 
         output = fake_stdout.getvalue()
         fake_stdout.close()
 
-        self.assertEqual(output, "I don't do much, yet.\n")
+        self.assertEqual(output, "")
+
+    def test_communal_cell_lives(self):
+        game = GameOfLife()
+        fake_stdout = io.StringIO()
+
+        with contextlib.redirect_stdout(fake_stdout):
+            game.run("1,1 1,2 2,1 2,2")
+
+        output = fake_stdout.getvalue()
+        fake_stdout.close()
+
+        self.assertEqual(output, "1,1 1,2 2,1 2,2")
+    
+    # def test_oscilating_cell(self):
+    #     game = GameOfLife()
+    #     fake_stdout = io.StringIO()
+
+    #     with contextlib.redirect_stdout(fake_stdout):
+    #         game.run("1,1 1,2 2,1 2,2")
+
+    #     output = fake_stdout.getvalue()
+    #     fake_stdout.close()
+
+    #     self.assertEqual(output, "1,1 1,2 2,1 2,2")
